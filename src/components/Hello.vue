ca07143af8a21eb603a4075ff83a3b733c4bd577
Alex <alexmoore94@hotmail.co.uk>
2017-03-10 14:01:20 +0000

    basic elements added

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,26 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    <h1>Pie Selector</h1>
+    <select>
+      <option value="steph">Steph</option>
+      <option value="alex">Alex</option>
+    </select>
+    <br>
+    <input type="radio" value="pleaseSelect" name="pies" id="pleaseSelect" v-model="picked">
+      <label for="pleaseSelect">Please Select</label>
       <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <input type="radio" value="theMatch" name="pies" id="theMatch" v-model="picked">
+      <label for="theMatch">The Match</label>
+      <br>
+    <input type="radio" value="theAussie" name="pies" id="theAusie" v-model="picked">
+      <label for="theAussie">The Aussie</label>
+      <br>
+    <button type="submit">Submit</button>
+    <span>Picked: {{ picked }}</span>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-    };
-  },
-};
+  export default {
+  };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
