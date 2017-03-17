c108f6d66d0e525b4ee1619efca33e302f5f1ffb
Steph Adams <steph@neontribe.co.uk>
2017-03-17 14:46:43 +0000

    Add pies to menu, add table to format pie list

--- src/components/Pie.vue
+++ src/components/Pie.vue
@@ -2,13 +2,14 @@
   <div class="pie">
     <h1>Hello, {{ sharedState.store.state.person }}!</h1>
     <p>Which pie would you like?</p>
-    <label>Pie Types:
-      <div v-for="pie in privateState.pies">
-        <input type="radio" :value="pie.name" name="pies" v-model="privateState.selection">
-        <label for="pies">{{ pie.name }}</label>
-      </div>
-    </label>
+    <table>
+      <tr v-for="pie in privateState.pies">
+        <td><input type="radio" :value="pie.name" name="pies" v-model="privateState.selection"></td>
+        <td><label for="pies">{{ pie.name }}</label></td>
+      </tr>
+    </table>
     <p v-show="privateState.selection"> Oh you fancy {{ privateState.selection }} do you {{ sharedState.store.state.person }}?</p>
+    <br>
     <button @click="onSubmit()">Submit</button>
   </div>
 </template>
@@ -22,8 +23,17 @@ export default {
     return {
       privateState: {
         pies: [
+          { name: 'The Brekkie' },
+          { name: 'The Veggie' },
+          { name: 'The Classic Aussie' },
           { name: 'The Match' },
-          { name: 'The Aussie' },
+          { name: 'The Aussie Deluxe' },
+          { name: 'The Pom' },
+          { name: 'The Grill' },
+          { name: 'The Flaming Galah' },
+          { name: 'The Bondi' },
+          { name: 'The Greek' },
+          { name: 'The Italian' },
         ],
         msg: 'Pie Selector',
         selection: false,
