56cca0d7d952e89217820862f2b5d4386ff96a9b
Alex <alexmoore94@hotmail.co.uk>
2017-03-17 16:55:50 +0000

    added store to stickortwst view

--- src/components/StickOrTwist.vue
+++ src/components/StickOrTwist.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<h1>It's Decision Time...</h1>
+		<h1>It's Decision Time {{ sharedState.store.state.person }}..</h1>
 		<p>So here is where the really big decisions are made.</p>
 		<p>You will now have two options. Your selection here may change the course of your life forever so please make it wisely.</p>
 		<p>Would you like to stick with your previously selected pie? Or would you like to twist and change it for another?</p>
@@ -10,5 +10,19 @@
 </template>
 
 <script>
-	
+import store from '../store';
+
+export default {
+  name: 'stickortwist',
+  data() {
+    return {
+      privateState: {
+
+      },
+      sharedState: {
+        store,
+      },
+    };
+  },
+};
 </script>
