9ab09eff94ba9a7ac5ffc61de00da86e28f44610
katjam <katja@neontribe.co.uk>
2017-03-19 17:17:05 +0000

    Sass images and prod build

--- src/components/StickOrTwist.vue
+++ src/components/StickOrTwist.vue
@@ -4,8 +4,8 @@
 		<p>So here is where the really big decisions are made.</p>
 		<p>You will now have two options. Your selection here may change the course of your life forever so please make it wisely.</p>
 		<p>Would you like to stick with your previously selected pie? Or would you like to twist and change it for another?</p>
-		<button @click="stickIt()">stick</button>
-		<button @click="twistIt()">twist</button>
+		<button>stick</button>
+		<button>twist</button>
 	</div>
 </template>
 
@@ -23,14 +23,6 @@ export default {
         store,
       },
     };
-  },
-  methods: {
-    stickIt() {
-      this.$router.push('selections');
-    },
-    twistIt() {
-      this.$router.push('pie');
-    },
   },
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -8,27 +8,15 @@ const store = {
     pie: PIE_DEFAULT_VALUE,
   },
   setPersonSelection(person) {
-    if (this.debug) {
-      console.log('setPersonSelection: ', person);
-    }
     this.state.person = person;
   },
   clearPersonSelection() {
-    if (this.debug) {
-      console.log('clearPersonSelection: ', this.state.person);
-    }
     this.state.person = PERSON_DEFAULT_VALUE;
   },
   setPieSelection(pie) {
-    if (this.debug) {
-      console.log('setPieSelection: ', pie);
-    }
     this.state.pie = pie;
   },
   clearPieSelection() {
-    if (this.debug) {
-      console.log('clearPieSelection: ', this.state.pie);
-    }
     this.state.pie = PIE_DEFAULT_VALUE;
   },
 };
