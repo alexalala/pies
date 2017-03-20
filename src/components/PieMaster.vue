988e36e7cd4f7c2963c97deed3e8d8b2fc5d7a20
Steph Adams <steph@neontribe.co.uk>
2017-03-20 15:15:29 +0000

    Add PieMaster component file

---
+++ src/components/PieMaster.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h1>Pie Master</h1>
+	</div>
+</template>
+
+<script>
+import store from '../store';
+
+export default {
+  name: 'PieMaster',
+  data() {
+    return {
+      sharedState: {
+        store,
+      },
+    };
+  },
+};
+</script>
