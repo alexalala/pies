38217942006ccb516d0025c0c4c078d3bead6dcc
James Thistlewood <jamesthistlewood@gmail.com>
2018-07-23 15:31:29 +0100

    fix 404 backend error

--- backend/server.sh
+++ backend/server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# always run this from the root directory of the project!
 
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' php|grep "install ok installed")
 echo Checking for php: $PKG_OK
